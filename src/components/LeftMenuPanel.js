3dd65b72103518170373aae39f708b5807790714
Khalid Abbas <khalid.abbas.iu@gmail.com>
2021-08-19 21:39:48 +0500

    added svgs icons

--- src/components/LeftMenuPanel.js
+++ src/components/LeftMenuPanel.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-export default class LeftMenuPanel extends Component {
-    static propTypes = {
-        prop: PropTypes
-    }
-
-    render() {
+let baseUrl=''
+export default class LeftMenuPanel extends Component {   
+     render() {
+        // console.log("baseUrl",baseUrl)
         return (
             <div>
-                <img src="./icons/textkit.png" />
+                <img src="./icons/docs-pile.svg"/>
             </div>
         )
     }
